526408b22417bac1a5d72354e02d21f3f990f132
sancar1 <tjsanchez7@gmail.com>
2014-04-16 23:38:34 -0400

    Changed collision borders to be attached to the camera rather than to the background; Changed mass of enemies to 5 instead of 1 to prevent player from pushing them around

---
+++ CatRedux/Assets/Scripts/FollowScript.js
@@ -0,0 +1,9 @@
+var target : GameObject;
+//var zpos : float;
+
+function Update()
+{
+//transform.position.y = target.transform.position.y+2;
+transform.position.x = target.transform.position.x+2;
+//transform.position.z = zpos;
+} 
